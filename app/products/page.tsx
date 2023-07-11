a6e87b090feb5c2d68fad9b645611f69baf19091
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-07-11 19:35:33 +0700

    Products

---
+++ app/products/page.tsx
@@ -0,0 +1,45 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Products',
+};
+
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+};
+
+async function getProducts() {
+  const res = await fetch('http://localhost:5000/products');
+  return res.json();
+}
+
+export default async function ProductList() {
+  const products: Product[] = await getProducts();
+
+  return (
+    <div className='py-10 px-10 '>
+      <table className='table w-full border-white'>
+        <thead className='bg-white'>
+          <tr className='text-black'>
+            <th>No</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody className='border border-white'>
+          {products.map((product, index) => (
+            <tr key={product.id} className='border-white text-white'>
+              <td>{index + 1}</td>
+              <td>{product.title}</td>
+              <td>{product.price}</td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
