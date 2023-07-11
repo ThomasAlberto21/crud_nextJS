90b2061a34e2f499b52fed8f45ce343e94fe662f
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-07-11 19:59:15 +0700

    .

--- app/products/addProduct.tsx
+++ app/products/addProduct.tsx
@@ -1,16 +1,41 @@
 'use client';
-import { useState } from 'react';
+import { SyntheticEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function AddProduct() {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
   const [modal, setModal] = useState(false);
+
+  const router = useRouter();
 
   const handleChange = () => {
     setModal(!modal);
   };
 
+  async function handleSubmit(e: SyntheticEvent) {
+    e.preventDefault();
+    await fetch('http://localhost:5000/products', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        title: title,
+        price: price,
+      }),
+    });
+
+    setTitle('');
+    setPrice('');
+    router.refresh();
+    setModal(false);
+  }
+
   return (
     <div>
-      <button className='btn bg-white text-black hover:bg-gray-400' onClick={handleChange}>
+      <button
+        className='btn bg-white text-black hover:bg-gray-400'
+        onClick={handleChange}
+      >
         Add New
       </button>
       <input
@@ -23,11 +48,13 @@ export default function AddProduct() {
       <div className='modal'>
         <div className='modal-box'>
           <h3 className='font-bold text-lg mb-5'>Add New Product</h3>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className='form-control'>
               <label className='label font-bold'>Title</label>
               <input
                 type='text'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
                 className='input w-full input-bordered'
                 placeholder='Product Name'
               />
@@ -36,13 +63,19 @@ export default function AddProduct() {
               <label className='label font-bold'>Price</label>
               <input
                 type='text'
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
                 className='input w-full input-bordered'
                 placeholder='Price'
               />
             </div>
 
             <div className='modal-action'>
-              <button type='button' className='btn bg-red-500' onClick={handleChange}>
+              <button
+                type='button'
+                className='btn bg-red-500'
+                onClick={handleChange}
+              >
                 Close
               </button>
               <button type='submit' className='btn btn-primary'>
