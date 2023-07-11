06a91ebc5de865c8b22735a909c1a83eb45e6ba6
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-07-11 20:16:48 +0700

    Finish

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'About',
-}
-
-export default function about() {
-  return <div>about</div>;
-}
--- app/posts/[...postId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Post Detail',
-};
-
-export default function PostDetail({ params }: { params: { postId: string } }) {
-  return <div>Post Detail {params.postId[0]}</div>;
-}
